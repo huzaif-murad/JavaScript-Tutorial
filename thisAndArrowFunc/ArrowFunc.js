e664d20f896b055711f26402bf1f8b0765999810
huzaif-murad <161587270+huzaif-murad@users.noreply.github.com>
2025-12-17 18:47:08 +0530

    some revision

--- thisAndArrowFunc/ArrowFunc.js
+++ thisAndArrowFunc/ArrowFunc.js
@@ -5,10 +5,24 @@
 // console.log(addNumbers(2, 3))
 
 
-const arr = [1, 2, 7, 8, 9]
-let arr1 = new Array(4, 56, 7)
-console.log(arr1);
+// const arr = [1, 2, 7, 8, 9]
+// let arr1 = new Array(4, 56, 7)
+// console.log(arr1);
 
-arr.forEach((val) => {
-    console.log(val)
-})
+// arr.forEach((val) => {
+//     console.log(val)
+// })
+
+let a = 10
+var b = 20
+
+function ex() {
+    console.log(b)
+    {
+        let c = 15
+
+    }
+    console.log(c)
+}
+
+ex()
