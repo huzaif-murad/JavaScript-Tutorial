a951a8df3a7284f2137db1461a0f57295a2f5686
huzaif-murad <161587270+huzaif-murad@users.noreply.github.com>
2025-12-29 13:07:35 +0530

    added number and decimal logic to calc project

--- Projects/Calculator/script.js
+++ Projects/Calculator/script.js
@@ -1,12 +1,46 @@
 
 
-function updateDisplay(e) {
+const display = document.querySelector('#display');
+console.log(display);
 
+const buttons = document.querySelector('#buttons');
 
+let currentVal = "0";
 
-    document.querySelector("#display").innerText += e.target.innerText;
-}
+buttons.addEventListener("click", function (e) {
 
-document.querySelector("#buttons").addEventListener("click", function (e) {
-    updateDisplay(e)
+    const type = e.target.dataset.type;
+    const value = e.target.dataset.value;
+    if (!e.target.classList.contains("btn")) return;
+
+    //Decimal handling
+    if (type === "special" && value === "decimal") {
+
+        //Decimal already exists
+        if (currentVal.includes('.')) {
+            return;
+        }
+        if (currentVal === "0") {
+
+            currentVal = "0."
+
+        } else {
+            currentVal += "."
+        }
+
+        display.innerText = currentVal;
+        return;
+    }
+    if (type === "number") {
+        if (currentVal === "0") {
+            display.innerText = e.target.dataset.value;
+            currentVal = display.innerText;
+        } else {
+            display.innerText = display.innerText + e.target.dataset.value;
+            currentVal = display.innerText;
+        }
+    }
+
+    console.log(e.target.dataset.type, e.target.dataset.value)
+
 })
