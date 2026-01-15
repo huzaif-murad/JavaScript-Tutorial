33fb90234c977f3ebd3efcbd80d3c066bbe6f391
huzaif-murad <161587270+huzaif-murad@users.noreply.github.com>
2026-01-15 23:14:40 +0530

    callback functions

--- Advance_01/callback.js
+++ Advance_01/callback.js
@@ -1,4 +1,15 @@
 
-function addition(a,b){
-    return a+b;
+function division(a, b) {
+    console.log(a / b);
+
 }
+function addition(a, b, division) {
+    division(a, b);
+    return a + b;
+
+}
+
+let a = 10;
+let b = 5;
+let ans = addition(a, b, division);
+console.log(ans);
