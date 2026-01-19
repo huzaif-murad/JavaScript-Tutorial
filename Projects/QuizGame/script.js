1a9ca7fa4dbebd3c6a0015d8fe656be2d789398c
huzaif-murad <161587270+huzaif-murad@users.noreply.github.com>
2026-01-19 10:40:39 +0530

    added some js

---
+++ Projects/QuizGame/script.js
@@ -0,0 +1,9 @@
+//start screen
+const startBtn = document.querySelector("#start-btn");
+
+//quiz screen 
+
+const questionText = document.querySelector("#question-text");
+const currentQuestion = document.querySelector('#current-question');
+const totalQuestions = document.querySelector('#total-questions');
+const score=document.querySelector('#score')
